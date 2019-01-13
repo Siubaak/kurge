9b8d6a5c7b2c2cb689f2c1f0042f094e5b22681f
Jimkwan <jimkwan.yau@gmail.com>
2019-01-13 10:27:48 +0800

    merge context when multi-invoke useContext

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,12 +4,7 @@ export const DATA_ID: string = 'data-kutid'
 export const PROXY_TARGET: symbol = Symbol('proxy_target')
 
 // vdom node reserved properties
-export const RESERVED_PROPS: {
-  [prop: string]: boolean
-} = {
-  key: true,
-  ref: true,
-}
+export const RESERVED_PROPS: { [prop: string]: boolean } = { key: true, ref: true }
 
 export const CUT_ON_REGEX: RegExp = /^on/
 const eventHandlers: string[] = Object.keys(window || {}).filter(key => CUT_ON_REGEX.test(key))
--- src/hooks/context.ts
+++ src/hooks/context.ts
@@ -1,5 +1,6 @@
 import observe from '../observer/index'
 import Dependency from '../observer/dependeny'
+import { PROXY_TARGET } from '../common/constants'
 
 export const context: { store: any } = { store: null }
 
@@ -7,7 +8,8 @@ export default function useContext(ctx: any) {
   if (Dependency.target) {
     throw new Error('please invoke useContext at top level outside all components')
   } else if (context.store) {
-    console.warn('Only once can useContext be invoked')
+    // merge duplicate contexts when invoke useContext multiple times
+    Object.assign(context.store[PROXY_TARGET], ctx)
   } else {
     context.store = observe(ctx)
   }
